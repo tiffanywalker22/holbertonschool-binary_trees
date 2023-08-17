702c29190cf4e31a15539c119a52e04983411044
tiffanywalker22 <tbcasper16@gmail.com>
2023-08-17 16:18:52 -0700

    initial commit

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates binary tree node
+ * @parent: parent node
+ * @value: value in new node
+ * Return: pointer to new node, or null
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
+}
