bcc86e4368929f62bf03947543581a69b7a9f308
tiffanywalker22 <tbcasper16@gmail.com>
2023-08-18 14:37:16 -0700

    commit nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes in a binary tree
+ * @tree: pointer to root node of tree to count the num of nodes
+ * Return: number of nodes, 0 if NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t node_count = 0;
+
+	if (!tree)
+		return (0);
+
+	if ((tree->left == NULL && tree->right != NULL) ||
+	    (tree->left != NULL && tree->right == NULL))
+		node_count++;
+	else if (node_count == 0 && (tree->left != NULL ||
+				     tree->right != NULL))
+		node_count++;
+	node_count += (binary_tree_nodes(tree->left) +
+		       binary_tree_nodes(tree->right));
+	return (node_count);
+}
